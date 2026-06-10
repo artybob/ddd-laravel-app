c241e574dd5728472ba12cfeb40908c70c9b03ff
ArtyBob <nova1hazard@gmail.com>
2026-06-10 14:06:13 +0700

    index test speedup query

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/slow-query', function () {
+    $start = microtime(true);
+    $total = DB::table('test_orders')
+        ->where('status', 'paid')
+        ->sum('total');
+    $time = round((microtime(true) - $start) * 1000, 2);
+
+    return response()->json([
+        'total' => $total,
+        'time_ms' => $time,
+        'index_used' => false,
+    ]);
 });
